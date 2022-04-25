c00cdcd1fa4cbdc4797c39a271c80e96ee399882
brunobruno <brett.bruno@yahoo.ca>
2022-04-25 14:16:01 -0700

    the calculator now works, but could work better
    - going to branch the code and optimize the display and code

--- main.js
+++ main.js
@@ -14,48 +14,45 @@ const buttons = document.querySelectorAll('.button');
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         
-        if(button.textContent == '=' && calcArray.length != 0) {
+        if((button.textContent == '=') && calcArray.length != 0) {
             result.textContent = calculate(calcArray);
         }
         else if(Number(calcArray[calcArray.length-1]) || (calcArray.length == 0 && parseInt(button.textContent))) {
             display.textContent = appendElement(calcArray, button.textContent);
-            console.log(calcArray);
         } 
         else if(!Number(calcArray[calcArray.length-1]) && parseInt(button.textContent)) {
             display.textContent = '' + appendElement(calcArray, button.textContent);
-            console.log(calcArray);
         }
     })
 })
 
-// CALCULATING
-// group digits together and separate numbers and operators
+let digits = [];
+let numbers = [];
+let operators = [];
+// takes input, returns array of numbers, and array of operators
 function calculate(calcArray) {
   let str = calcArray.toString().split(',').join('');
-  let digits = [];
-  let numbers = [];
-  let operators = [];
 
   console.log(calcArray);
 
-// NEEDS FIXING
   for(let i = 0; i < str.length; i++) {
   	digits.push(str[i])
   	if(str[i]=='+' || str[i]=='-' || 
        str[i]=='÷' || str[i]=='x' ||
        str[i]=='=') {
        operators.push(str[i]);
-       digits.splice(digits.length-1,1); 
+       digits.splice(digits.length-1,1); // removes operator
        numbers.push((digits.toString().split(',').join('')).slice(numbers.toString().split(',').join('').length));
     }
-    console.log(digits);
-    console.log(numbers);
-    console.log(operators);
-  } 
-  // NEEDS FIXING
+    else if(i == str.length - 1) {
+       numbers.push((digits.toString().split(',').join('')).slice(numbers.toString().split(',').join('').length));
+    }
+  }
+
+  /////////////////////////////////////////////////////
 
   const result = function(numbers, operators) {
-    for(let i = 0; i < operators.length-1; i++) {
+    for(let i = 0; i < operators.length; i++) {
       if(operators[i]=='+') {
         numbers[0] = add(numbers[0],numbers[1]);
         numbers.splice(1,1);
